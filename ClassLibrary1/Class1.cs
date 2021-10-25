e87ac55960635a75e34b0526d4646a32e873bec8
Panda <44100281+Madzhidov@users.noreply.github.com>
2021-10-25 13:01:31 +0500

    Add files via upload

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -1,16 +1,18 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace ClassLibrary1
 {
     public class Class1
     {
         const string UnwantedSigns = "<>.,?!-/*”“'\"";
-         private Dictionary<string, int> met(string FileContents)
+         private Dictionary<string, int> Chet(string FileContents)
         {
             foreach (char c in UnwantedSigns)
             {
@@ -32,5 +34,32 @@ namespace ClassLibrary1
 
             return WordCounts;
         }
+
+
+        public static Dictionary<string, int> Chet_2(string FileContents)
+        {
+
+
+            foreach(char c in UnwantedSigns)
+            {
+                FileContents = FileContents.Replace(c, ' ');
+                GC.Collect();
+            }
+            FileContents = FileContents.Replace(Environment.NewLine, " ");
+            string[] Words = FileContents.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            FileContents = string.Empty;
+            GC.Collect();
+            var WordCounts = new System.Collections.Concurrent.ConcurrentDictionary<string, int>();
+            Parallel.ForEach(Words, line =>
+           {
+               WordCounts.AddOrUpdate(line, 1, (_, x) => x + 1);
+           });
+            Words = new string[1];
+            var WordCounts2 = WordCounts.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            GC.Collect();
+            
+            return WordCounts2;
+        }
+
     }
 }
