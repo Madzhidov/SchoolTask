6f654c6ec83706e3561c1ddc1d8499002cf197af
Panda <44100281+Madzhidov@users.noreply.github.com>
2021-10-25 19:43:22 +0500

    Add files via upload

---
+++ MyCounter/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+
+
+namespace MyCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FileContents = File.ReadAllText(args[0]);
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            using (var client = new ServiceReference1.Service1Client())
+            {
+                var WordCounts = client.countWord(FileContents);
+                List<Tuple<int, string>> WordStats = WordCounts.Select(x => new Tuple<int, string>(x.Value, x.Key)).ToList();
+                WordStats.Sort((x, y) => y.Item1.CompareTo(x.Item1));
+                foreach (Tuple<int, string> t in WordStats)
+                    File.AppendAllText(args[1], t.Item2 + " " + t.Item1 + Environment.NewLine);
+            }
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WcfService/Service1.svc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfService
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "Service1" в коде, SVC-файле и файле конфигурации.
+    // ПРИМЕЧАНИЕ. Чтобы запустить клиент проверки WCF для тестирования службы, выберите элементы Service1.svc или Service1.svc.cs в обозревателе решений и начните отладку.
+    public class Service1 : IService1
+    {
+        public Dictionary<string, int> countWord(string FileContents)
+        {
+
+            const string UnwantedSigns = "<>.,?!-/*”“'\"";
+            foreach (char c in UnwantedSigns)
+            {
+                FileContents = FileContents.Replace(c, ' ');
+                GC.Collect();
+            }
+            FileContents = FileContents.Replace(Environment.NewLine, " ");
+            string[] Words = FileContents.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            FileContents = string.Empty;
+            GC.Collect();
+            var WordCounts = new System.Collections.Concurrent.ConcurrentDictionary<string, int>();
+            Parallel.ForEach(Words, line =>
+            {
+                WordCounts.AddOrUpdate(line, 1, (_, x) => x + 1);
+            });
+            Words = new string[1];
+            var WordCounts2 = WordCounts.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            GC.Collect();
+
+            return WordCounts2;
+        }
+    }
+}
